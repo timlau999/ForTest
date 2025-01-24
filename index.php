8ca77cc811d36a24c9c8b4f00aa624c18b144419
timlau999 <1061767565qq@gmail.com>
2025-01-23 22:14:10 -0800

    Ceate php files

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("db/dbconnect.php");  
+error_reporting(0);  
+session_start(); 
+
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=urf-8" data-charset="utf8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, viewport-fit=cover, interactive-widget=resizes-visual">
+    <meta name="description" content="">
+    <link rel="icon" href="#">
+    <title>Home || Smart Restaurant System with AI Enhancement</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+
+
+<body >
+    
+
+</body>
+
+
+</html>
