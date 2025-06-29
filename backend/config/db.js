073f9a222c3bef0db2997ff7f7db893d5720289b
timlau999 <1061767565qq@gmail.com>
2025-06-29 04:01:57 +0000

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,21 @@
 // /MyFYP_HD/backend/config/db.js
 import { Sequelize } from 'sequelize';
 /*
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  timezone: '+08:00',
+});
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('DB Connected');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+*/
 const sequelize = new Sequelize('fyp-db', 'admin1', 'FYPdb2025', {
   host: 'hhh-fyp2025.mysql.database.azure.com',
   dialect: 'mysql',
@@ -15,10 +30,10 @@ export const connectDB = async () => {
     console.error('Unable to connect to the database:', error);
   }
 };
-*/
+
 // for local test
 
-const sequelize = new Sequelize('fypdb1', 'root', '', {
+/*const sequelize = new Sequelize('fypdb1', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3307,
@@ -33,6 +48,6 @@ export const connectDB = async () => {
     console.error('Unable to connect to the database:', error);
   }
 };
-
+*/
 
 export default sequelize; 
